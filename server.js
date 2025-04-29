c49e4685be3a410ab209037a716b0740f5ef8e2d
room1202 <room1202itc@gmail.com>
2025-04-29 01:12:21 -0400

    server.js

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
-app.use(cors({
-    origin: 'https:///budgetwise.freehostspace.com/dashboard
-        }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // POST endpoint for generating AI responses
